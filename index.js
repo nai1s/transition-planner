c24a30b6cd720cea70e58f773cae636e02e17960
swarmidentity <82755646+swarmidentity@users.noreply.github.com>
2022-05-09 20:31:59 -0700

    Add azure pipelines and docker compose

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 8080;
+const port = 3000;
 
 // sendFile will go here
 var static = require('node-static');
